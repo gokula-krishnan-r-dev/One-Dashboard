88e21e406e903dab9d491c699136495bc784974c
Farihul Rouf <farihulrouf@gmail.com>
2023-01-02 20:02:44 +0700

    help center

--- components/HelpCenter.js
+++ components/HelpCenter.js
@@ -1,12 +1,17 @@
 import React from "react"
 import Image from "next/image"
+import { BsQuestionLg } from "react-icons/bs";
+
 const HelpCenter = () => {
     return (
         <React.Fragment>
-            <div className="max-w-sm relative bg-blue-50">
+            <div className="max-w-sm relative bg-blue-50 relative">
+                <div className="absolute -top-6 left-1/2 transform -translate-x-1/2 rounded-full bg-white p-3 rounded border">
+                    <BsQuestionLg size={28} className="text-white bg-blue-500 rounded-full p-1" />
+                </div>
                 <Image src='/img/Ellipse10.png' alt="pic" width={75} height={75} className="absolute top-0 left-0" />
                 <Image src='/img/Ellipse11.png' alt="pic" width={75} height={75} className="absolute bottom-0 right-0" />
-                <div className="flex flex-col justify-center items-center p-4 space-y-4">
+                <div className="flex flex-col justify-center items-center p-4 space-y-4 pt-12 pb-12">
                     <h3 className="text-md text-center">Help Center</h3>
                     <p className="text-gray-400 text-center text-[13px]">
                          Having trouble in 3D Content Studio.
