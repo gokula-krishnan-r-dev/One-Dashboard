6f3ab7226d2df00bfb2a86f730bfe1b2f1e0525f
Farihul Rouf <farihulrouf@gmail.com>
2023-01-03 16:45:22 +0700

    font 2.1

--- components/card/DetailProject.js
+++ components/card/DetailProject.js
@@ -13,7 +13,7 @@ const DetailProject = (props) => {
                     <Image src={itemData.img} width={91} height={91} alt="product" className="w-1/4" />
                     <div className="flex items-center justify-between w-1/2 font-medium">
                         <div className="full">
-                            <p className="text-black py-2 font-semibold text-md">{itemData.title}</p>
+                            <p className="text-black py-2 font-semibold text-base">{itemData.title}</p>
                             <div className="w-36 bg-[#F9C152] py-2 text-[12px] text-white text-center rounded-full">{itemData.status}</div>
                         </div>
                     </div>
@@ -22,7 +22,7 @@ const DetailProject = (props) => {
                 <div className="flex flex-col w-full text-sm font-medium">
                    <ul className="flex w-full justify-between p-2">
                         <li className="text-[#131313]">Order By</li>
-                        <li className="text-blue-500 text[##007AFF]">Angeline Lee</li>
+                        <li className="text-blue-500 text-blue-500">Angeline Lee</li>
                    </ul>
                    <ul className="flex w-full justify-between p-2">
                         <li className="text-[#131313]">Delivery Date</li>
