4f1db02d8b65fc609ca1e1fcdcd192cea3642e51
Farihul Rouf <farihulrouf@gmail.com>
2023-01-04 08:42:27 +0700

    nav

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import { useRouter  } from 'next/router';
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-  if(router.asPath == '/login' || router.asPath == '/signup'){
+  if(router.asPath == '/login' || router.asPath == '/signup' || router.asPath == '/'){
     return (
       <Component {...pageProps} />
     )
