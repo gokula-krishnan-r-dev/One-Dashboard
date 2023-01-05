4748fabb042a2178d81d09a871e1e49d25499467
Farihul Rouf <farihulrouf@gmail.com>
2023-01-05 15:17:49 +0700

    client

--- pages/client/index.js
+++ pages/client/index.js
@@ -16,19 +16,19 @@ const Client = () => {
         <div className="flex flex-col w-full">
           <div className="overflow-x-auto font-play">
             <p className="text-gray-400">Overview</p>
-            <div className="py-2 inline-block min-w-full flex justify-between items-center">
-              <h3 className="text-4xl">My Projects</h3>
-              <div className="flex items-center space-x-3">
-                <div className="relative text-gray-600 focus-within:text-gray-400">
+            <div className="py-2 block min-w-full md:flex md:justify-between md:items-center md:gap-2 flex-wrap">
+              <h3 className="md:text-3xl text-xl">My Projects</h3>
+              <div className="md:flex md:items-center md:flex-wrap md:gap-2 block">
+                <div className="w-full md:w-44 border relative mt-2">
                   <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                     <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
                       <BsSearch />
                     </button>
                   </span>
-                  <input type="search" name="q" className="py-3 border text-sm text-white pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autoComplete="off" />
+                  <input type="text" placeholder="search" className="pl-10 py-2.5 w-full" />
                 </div>
-                <div className="inline-block relative w-36 text-gray-400">
-                  <select className="block appearance-none w-full bg-white border px-4 py-2.5">
+                <div className="w-full md:w-44 border relative mt-2">
+                  <select className="block appearance-none w-full bg-white px-4 py-2.5">
                     <option>This Week</option>
                     <option>Option 2</option>
                     <option>Option 3</option>
@@ -37,30 +37,38 @@ const Client = () => {
                     <BsChevronDown />
                   </div>
                 </div>
-                <div className="inline-block relative w-40 text-gray-400">
-                  <select className="block appearance-none w-full bg-white border px-4 py-2.5">
-                    <option>All</option>
-                    <option>Option 2</option>
+                <div className="w-full md:w-44 border relative mt-2">
+                  <select className="block appearance-none w-full px-4 py-2.5 bg-blue-500 text-white">
+                    <option className="text-red-500">All</option>
+                    <option>Draft</option>
+                    <option>Pending Review</option>
+                    <option>Pending Quote</option>
+                    <option>Pending Payment</option>
+                    <option>In Production</option>
+                    <option>In Repair</option>
+                    <option>Delivered</option>
+                    <option>Completed</option>
                   </select>
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <BsChevronDown />
+                    <BsChevronDown className="text-white font-medium" />
                   </div>
                 </div>
-                <button className="bg-blue-500 text-white py-2.5 px-3" onClick={() => router.push('/client/create')}>New Project Request</button>
+                <div className="w-full md:w-60 mt-2">
+                  <button className="bg-blue-500 text-white py-2.5 w-full" onClick={() => router.push('/client/create')}>New Project Request</button>
+                </div>
               </div>
             </div>
           </div>
           <div className="overflow-auto">
-            <div className="py-2 inline-block h-[900px] overflow-y-auto">
-              <div className="flex flex-wrap gap-4 mt-8 items-center justify-between">
+              <div className="flex flex-wrap gap-4 mt-8 items-center lg:justify-start justify-center">
                 {products.map((product, index) => (
                   <div key={index}>
                     <CardProject product={product} />
                   </div>
                 ))}
               </div>
-            </div>
           </div>
+          
         </div>
       </div>
     </React.Fragment>
