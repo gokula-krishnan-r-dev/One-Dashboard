46f6392ffd9e23c6f7b324405795b8991c5f8e99
Farihul Rouf <farihulrouf@gmail.com>
2023-01-05 08:42:26 +0700

    orders

--- pages/admin/orders/index.js
+++ pages/admin/orders/index.js
@@ -11,23 +11,19 @@ const Order = () => {
     <React.Fragment>
       <div className="p-4 pt-4 bg-[#FAFAFA] h-full">
         <div className="flex flex-col w-full">
-          <div className="overflow-x-auto font-play">
             <p className="text-gray-400">Overview</p>
-            <div className="py-2 inline-block min-w-full flex justify-between items-center">
+            <div className="py-2 inline-block min-w-full md:flex md:justify-between md:items-center">
               <a className="text-4xl py-4"><Link href='/customers'>Customers</Link></a>
-              <div className="flex items-center space-x-3">
-                <div className="relative text-gray-600 focus-within:text-gray-400">
+              <div className="w-full md:w-44 border relative mt-2">
                   <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                     <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
                       <BsSearch />
                     </button>
                   </span>
-                  <input type="search" name="q" className="py-3 border text-sm text-white pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autoComplete="off" />
+                  <input type="text" placeholder="search" className="pl-10 py-2.5 w-full" />
                 </div>
-              </div>
             </div>
-          </div>
-          <div className="overflow-x-auto p-2 border bg-white">
+            <div className="overflow-x-auto p-2 border bg-white">
             <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
               <div className="overflow-hidden">
                 <table className="min-w-full">
@@ -92,6 +88,7 @@ const Order = () => {
               </div>
             </div>
           </div>
+            
         </div>
       </div>
     </React.Fragment>
