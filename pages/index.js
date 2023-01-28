7ff00e8607d222e76b31d9525345afe4ea1bbd54
Farihul Rouf <farihulrouf@gmail.com>
2023-01-28 15:41:29 +0700

    home pages

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,42 @@
 import { products } from "../components/data/dataContents";
-import CardOrder from  "../components/card/CardOrder"
 import { BsSearch, BsChevronDown } from "react-icons/bs";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import { SiGooglechat } from "react-icons/si";
+import NavBar from "../components/NavBar";
 import React from "react";
 //BsSearch
 const Home = () => {
   const router = new useRouter()
   return (
     <React.Fragment>
-      <div className="p-4 pt-4 bg-[#FAFAFA] relative">
-        <div className='p-4 fixed  bottom-2 right-2 bg-blue-600 rounded-full'>
-           <SiGooglechat className="text-white" size={32} />
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+          <span className="font-semibold text-xl tracking-tight">Multivespro</span>
         </div>
-        <div className="flex flex-col w-full">
-          <div className="overflow-x-auto font-play">
-            <p className="text-gray-400">Overview</p>
-            <div className="py-2 block min-w-full md:flex md:justify-between md:items-center md:gap-2 flex-wrap">
-              <h3 className="md:text-3xl text-xl">My Projects</h3>
-              <div className="md:flex md:items-center md:flex-wrap md:gap-2 block">
-                <div className="w-full md:w-44 border relative mt-2">
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                    <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
-                      <BsSearch />
-                    </button>
-                  </span>
-                  <input type="text" placeholder="search" className="pl-10 py-2.5 w-full" />
-                </div>
-                <div className="w-full md:w-44 border relative mt-2">
-                  <select className="block appearance-none w-full bg-white px-4 py-2.5">
-                    <option>This Week</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <BsChevronDown />
-                  </div>
-                </div>
-                <div className="w-full md:w-44 border relative mt-2">
-                  <select className="block appearance-none w-full px-4 py-2.5 bg-blue-500 text-white">
-                    <option className="text-red-500">All</option>
-                    <option>Draft</option>
-                    <option>Pending Review</option>
-                    <option>Pending Quote</option>
-                    <option>Pending Payment</option>
-                    <option>In Production</option>
-                    <option>In Repair</option>
-                    <option>Delivered</option>
-                    <option>Completed</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <BsChevronDown className="text-white font-medium" />
-                  </div>
-                </div>
-                <div className="w-full md:w-60 mt-2">
-                  <button className="bg-blue-500 text-white py-2.5 w-full" onClick={() => router.push('/client/create')}>New Project Request</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="overflow-auto">
-              <div className="flex flex-wrap gap-4 mt-8 items-center lg:justify-start justify-center">
-                {products.map((product, index) => (
-                  <div key={index}>
-                    <CardOrder product={product} />
-                  </div>
-                ))}
-              </div>
-          </div>
-          
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          </button>
         </div>
-      </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Faq
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              News
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+              Product
+            </a>
+          </div>
+          <div>
+            <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"><Link href='/login'>Sign</Link></a>
+          </div>
+        </div>
+      </nav>
     </React.Fragment>
 
   );
